f0370d3639b7166a40144c196d4776a67904c063
brightokike@gmail.com <brightokike@gmail.com>
2020-07-31 11:21:01 +0300

    used webpacker to install react, setup the basic components for the frontend using react

---
+++ app/javascript/component/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Airlines from './airlines/Airlines';
+import Airline from './airline/Airline';
+
+const App = () => {
+	return (
+		<Switch>
+			<Route exact path="/" component={Airlines} />
+			<Route exact path="/airlines/:slug" component={Airline} />
+		</Switch>
+	)
+
+}
+
+export default App;
---
+++ app/javascript/component/airlines/Airlines.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect, Fragment } from 'react'
+import axios from 'axios'
+
+const Airlines = () => {
+	const [airlines, setAirlines] = useState([])
+
+	useEffect(() => {
+
+		axios.get('/api/v1/airlines.json')
+		.then(res => {
+			setAirlines(res.data.data)
+		})
+		.catch(res => console.log(res))
+	}, [airlines.length])
+
+	const list = airlines.map(item => {
+		return (<li key={item.attributes.name}>{item.attributes.name} </li>)
+	})
+
+	return(
+		<Fragment>
+			<div> This is the Airlines#index of our app</div>
+			<ul> {list} </ul>
+		</Fragment>
+	)
+}
+
+export default Airlines;
