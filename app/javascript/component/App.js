b0f5615f59c32989963029c21019ae4e00a968bb
brightokike@gmail.com <brightokike@gmail.com>
2020-07-31 14:29:25 +0300

    implemented airline show view, and added so UI designs to it

--- app/javascript/component/App.js
+++ app/javascript/component/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './app.css'
 import { Route, Switch } from 'react-router-dom';
 import Airlines from './airlines/Airlines';
 import Airline from './airline/Airline';
--- app/javascript/component/airline/Airline.js
+++ app/javascript/component/airline/Airline.js
@@ -1,7 +1,68 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
+import Header from './Header'
+import styled from 'styled-components'
 
-const Airline = () => {
-	return (<div> This is the Airline#show view of our app</div>)
+
+const Wrapper = styled.div`
+	margin-left: auto;
+	margin-right: auto;
+	display: grid;
+	grid-template-columns: repeat(2, 1fr);
+`
+const Column = styled.div`
+	background: #fff;
+	height: 100vh;
+	overflow: scroll;
+
+	&:last-child {
+		background: #000;
+	}
+`
+const Main = styled.div`
+	left-padding: 50px;
+`
+
+const Airline = (props) => {
+	const [airline, setAirline] = useState()
+	const [review, setReview] = useState()
+	const [loaded, setLoaded] = useState(false)
+
+	useEffect(() =>{
+		const slug = props.match.params.slug
+		const url = `/api/v1/airlines/${slug}`
+		
+
+		axios.get(url)
+		.then(res => {
+			setAirline(res.data)
+			setLoaded(true)
+		})
+		.catch(err => console.log(err))
+	}, [])
+
+	return (
+		<Wrapper>
+			<Column>
+				<Main>
+					{
+						loaded &&
+						<Header 
+							attributes={airline.data.attributes}
+							reviews={airline.included}
+						/>
+					}
+				</Main>
+				<div className="review"></div> 		
+			</Column> 
+			<Column>
+				<div className="review-form">[Reveiw form goes here...] </div> 
+			</Column> 
+			
+			
+
+		</Wrapper>
+	)
 }
 
 export default Airline;
